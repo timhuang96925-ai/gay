530f2ab2e1509f60f331447a7ec8f212f1691b93
timhuang96925-ai <timhuang96925@apps.ntpc.edu.tw>
2025-10-03 10:02:28 +0800

    gay

--- guest.js
+++ guest.js
@@ -3,12 +3,6 @@ const guessField = document.querySelector(".guessField");
 const result = document.querySelector(".result");
 const count = document.querySelector(".count");
 
-let countNum =0;   //廣域變數
-function checkGuess() {
-    countNum++;
-    count.textContent = "猜測次數："+countNum;
-    //guessField.focus();       //游標焦點預設在輸入欄位裡
-}
 guessSubmit.addEventListener("click", checkGuess);   //當按鈕被點擊，執行函式
 
 let randomNumber = Math.floor(Math.random()*100);
@@ -16,6 +10,12 @@ console.log("觀察隨機的數字：", randomNumber);
 
 //比較數字
 const userGuess = Number(guessField.value);  //取得欄位值，並轉為數字
+
+let countNum =0;   //廣域變數
+function checkGuess() {
+    countNum++;
+    count.textContent = "猜測次數："+countNum;
+    //guessField.focus();       //游標焦點預設在輸入欄位裡
 if  (  userGuess === randomNumber ) {
     result.textContent = "猜測結果：Congratulations!" ;
 }
@@ -25,3 +25,7 @@ else if (userGuess  < randomNumber ) {
 else if (userGuess  >  randomNumber ) {
     result.textContent = "猜測結果：數字太大!";
 }
+
+
+}
+
